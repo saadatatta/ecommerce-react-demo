a234e9023b23d52167912d88cd237b434dc3a378
MuhammadSaadat\Muhammad Saadat <muhammadsaadat42@gmail.com>
2020-06-21 20:30:23 +0500

    Added higher order component to menu item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homePage/homePage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div>
-      <HomePage></HomePage>
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage}></Route>
+      <Route exact path="/hats" component={HatsPage}></Route>
+    </Switch>
   );
 }
 
--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -1,20 +1,32 @@
 import React from "react";
 
 import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({
+  title,
+  imageUrl,
+  size,
+  linkUrl,
+  match,
+  location,
+  history,
+}) => {
   return (
-    <div className={`menu-item ${size}`}>
+    <div
+      className={`menu-item ${size}`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       ></div>
       <div className="content">
-        <div className="title">{title}</div>
+        <div className="title">{title.toUpperCase()}</div>
         <span className="subtitle">SHOP NOW</span>
       </div>
     </div>
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
