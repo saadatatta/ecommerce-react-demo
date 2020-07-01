07ae9f5de32a8bcc616c85a6e492dc088c0fc923
MuhammadSaadat\Muhammad Saadat <muhammadsaadat42@gmail.com>
2020-07-01 20:06:16 +0500

    signin form basic added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homePage/homePage.component";
 import { Switch, Route } from "react-router-dom";
 import Shop from "./pages/shop/shop.component";
 import Header from "./pages/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/shop" component={Shop}></Route>
+        <Route exact path="/signin" component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
